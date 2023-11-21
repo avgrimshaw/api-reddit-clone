911bb46b01b507348b14c32f9787030529d8fb34
avgrimshaw <adamvgrimshaw@outlook.com>
2023-11-21 14:11:19 +0000

    add refactor to 'GET /api' request test

--- __tests__/api.test.js
+++ __tests__/api.test.js
@@ -7,41 +7,9 @@ describe("GET /api", () => {
       .get("/api")
       .expect(200)
       .then(({ body }) => {
-        const endpointsJson = JSON.parse(body.endpoints);
-        const endpointPaths = Object.keys(endpointsJson);
-        endpointPaths.forEach((endpointPath) => {
-          if (endpointPath.slice(-4) === "/api") {
-            expect(endpointsJson[endpointPath]).toMatchObject({
-              description: expect.any(String),
-            });
-          } else {
-            expect(endpointsJson[endpointPath]).toEqual({
-              description: expect.any(String),
-              queries: expect.any(Array),
-              exampleResponse: expect.any(Object),
-            });
-          }
-        });
-      });
-  });
-  it("200: 'exampleResponse' should contain correct property key and an array value which it''s object item should contain correct properties", () => {
-    return request(app)
-      .get("/api")
-      .expect(200)
-      .then(({ body }) => {
-        const endpointsJson = JSON.parse(body.endpoints);
-        const endpointPaths = Object.keys(endpointsJson);
-        endpointPaths.forEach((endpointPath) => {
-          const endpointPathSplit = endpointPath.split("/");
-          const currentPath = endpointPathSplit.at(-1);
-          const endpoint = endpointsJson[endpointPath];
-          if (currentPath !== "api") {
-            expect(endpoint["exampleResponse"]).toHaveProperty(
-              [currentPath],
-              expect.any(Array)
-            );
-          }
-        });
+        const endpointsJson = require("../endpoints.json");
+        const returned = JSON.parse(body.endpoints);
+        expect(endpointsJson).toMatchObject(returned);
       });
   });
 });
