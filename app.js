d4df80bf32ea28d31fa1dd24ab1c7a8e2a0eba69
avgrimshaw <adamvgrimshaw@outlook.com>
2023-11-21 12:24:04 +0000

    add TDD for 'GET /api' endpoint

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const { getAllTopics } = require("./controllers/topics.controller");
 const { handleServerErrors } = require("./errors");
+const { getEndpointsJson } = require("./controllers/api.controller");
 const app = express();
 
 app.get("/api/topics", getAllTopics);
+
+app.get("/api", getEndpointsJson);
 
 app.use(handleServerErrors);
 
